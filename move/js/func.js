199a7233557bb13158c009025e41e5ee80d790d1
hirokihjkl <57675646+hirokihjkl@users.noreply.github.com>
2020-08-05 20:00:48 +0900

    mo
    move

---
+++ move/js/func.js
@@ -0,0 +1,17 @@
+var frames = document.getElementsByClassName('anime')[0].getElementsByTagName('img');
+viewFrame();
+
+function viewFrame(frame_no = -1) {
+    if (frames[frame_no]) {
+        frames[frame_no].style.display = 'none';
+    }
+    frame_no++;
+    if (frames[frame_no]) {
+        frames[frame_no].style.display = 'block';
+    } else {
+        frames[0].style.display = 'block';
+        frame_no = 0;
+    }
+    var msec = document.getElementById('anime_speed').value;
+    setTimeout(function() { viewFrame(frame_no); }, msec);
+}
